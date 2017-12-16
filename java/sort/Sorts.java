99302e1ddae43dde8627b47fe82868201744ac22
rgabeflores <rgabeflores@gmail.com>
2017-12-16 03:25:48 -0800

    Minor Revision

--- java/sort/Sorts.java
+++ java/sort/Sorts.java
@@ -1,6 +1,6 @@
 
 public class Sorts {
-	public static int[] insertionSort(int[] a) {
+	public static int[] selectionSort(int[] a) {
 		for(int i = 0; i < a.length; i++) {
 			for(int j = i + 1; j < a.length; j++) {
 				if (a[i] <= a[j]) continue;
