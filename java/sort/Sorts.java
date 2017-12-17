626425ae29a4ea090b0714b9e43b9088fb277370
rgabeflores <rgabeflores@gmail.com>
2017-12-16 16:01:50 -0800

    Added Insertion Sort

--- java/sort/Sorts.java
+++ java/sort/Sorts.java
@@ -13,4 +13,17 @@ public class Sorts {
 		}
 		return a;
 	}
+	public static int[] insertionSort(int[] a) {
+		int temp, j;
+		for(int i = 1; i < a.length; i++) {
+			temp = a[i];
+			j = i;
+			while(j > 0 && temp < a[j - 1]) {
+				a[j] = a[j-1];
+				j--;
+			}
+			a[j] = temp;
+		}
+		return a;
+	}
 }
