f8bcf8ad3173260eab5b7bbf351e4c1fd2c62bd4
rgabeflores <rgabeflores@gmail.com>
2017-12-19 00:40:13 -0800

    Initial Commit

---
+++ python/math-functions/functions.py
@@ -0,0 +1,19 @@
+'''
+	@author Gabriel Flores
+	Various mathematical functions concatenated into one script.
+'''
+
+import check_prime
+import decimal_binary
+import euclidean_algorithm
+import power_mod
+import reverse_number
+
+def main():
+
+	ui_menu = "(INSERT MENU)" # Create menu 
+	print(ui_menu)
+	choice = int(input("Enter a function to use: "))
+
+if __name__ == "__main__":
+	main()
