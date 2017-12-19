90e70b9bd9b9faa207a4d76435ac75712af02d8c
rgabeflores <rgabeflores@gmail.com>
2017-12-19 15:50:54 -0800

    Created Modules

--- python/math-functions/functions.py
+++ python/math-functions/functions.py
@@ -10,6 +10,7 @@ import euclidean_algorithm
 import power_mod
 import reverse_number
 
+options = ["(1) Check Prime", "(2) Decimal/Binary Conversions", "(3) Greatest Common Divisor", "(4) Power Mod Calculation", "(5) Reverse Integer"]
 
 def switch(x):
 	exec(str({
@@ -20,43 +21,50 @@ def switch(x):
 		5: reverse_number
 	}[x].main()))
 
-def main():
+def to_continue():
+	while True:
+		again = input("Would you like to use another function? (Y/N)\n")
+		if again.upper() == "Y":
+			return True
+		elif again.upper() == "N":
+			return False
+		else:
+			print("\n\n    Please enter a valid option.\n")
+			continue
 
-	options = ["(1) Check Prime", "(2) Decimal/Binary Conversions", "(3) Greatest Common Divisor", "(4) Power Mod Calculation", "(5) Reverse Integer"]
+def get_user_choice():
 
 	ui_menu = "\n    "
 
 	for option in options:
 		ui_menu += option + "\n    "
 
+	while True:
+		try:
+			print(ui_menu)
+			choice = int(input("Enter a function to use:  "))
+			if choice <= len(options) and choice > 0:
+				break
+			else:
+				raise ValueError("Number too large.")
+		except ValueError as e:
+			print("\n\n    Please enter a valid choice.\n")
+			continue
+
+	return choice
+
+def main():
+
 	cont = True
 
 	while cont:
-		while True:
-			try:
-				print(ui_menu)
-				choice = int(input("Enter a function to use:  "))
-				if choice <= len(options) and choice > 0:
-					break
-				else:
-					raise ValueError("Number too large.")
-			except ValueError as e:
-				print("\n\n    Please enter a valid choice.\n")
-				continue
+		
+		choice = get_user_choice()
 
 		print("\n\n")
 		switch(choice)
 
-		while True:
-			again = input("Would you like to use another function? (Y/N)\n")
-			if again.upper() == "Y":
-				break
-			elif again.upper() == "N":
-				cont = False
-				break
-			else:
-				print("\n\n    Please enter a valid option.\n")
-				continue
+		cont = to_continue()
 
 	print("\n\n")
 
