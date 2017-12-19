f9affb78351ce4fbd4406db6f369f2662374bc9a
rgabeflores <rgabeflores@gmail.com>
2017-12-19 15:54:59 -0800

    Added Option to Repeat

--- python/math-functions/check_prime.py
+++ python/math-functions/check_prime.py
@@ -13,12 +13,38 @@ def is_prime(x):
 			return False
 	return True
 
+def to_continue():
+	while True:
+		again = input("    Would you like to check another integer? (Y/N) ")
+		if again.upper() == "Y":
+			return True
+		elif again.upper() == "N":
+			return False
+		else:
+			print("\n\n    Please enter a valid option.\n")
+			continue
+
 def main():
-	a = int(input("Enter an integer to check if it is prime: "))
-	if is_prime(a):
-		print("\n",a,"is a prime number.\n")
-	else:
-		print("\n",a,"is not a prime number.\n")
+
+	cont = True
+
+	while cont:
+		try:
+			print("\n\n")
+			a = int(input("    Enter an integer to check if it is prime: "))
+			if is_prime(a):
+				print("\n   ",a,"is a prime number.\n")
+			else:
+				print("\n   ",a,"is not a prime number.\n")
+		except ValueError as e:
+			print("\n\n    Please enter a valid choice.\n")
+			continue
+
+		cont = to_continue()
+
+	print("\n\n")
+
+	
 
 if __name__ == "__main__":
 	main()
